8d5f4727840d20738e16825d3fae73ad1323dcb7
billguo07@yahoo.ca <billguo07@yahoo.ca>
2023-01-22 18:59:30 -0700

    fewfwef

--- ml_utils.py
+++ ml_utils.py
@@ -4,9 +4,6 @@ import math
 import sklearn.datasets
 import ipywidgets as widgets
 
-from IPython.display import display
-import pandas as pd
-
 ##Seaborn for fancy plots. 
 #%matplotlib inline
 import matplotlib.pyplot as plt
@@ -29,6 +26,8 @@ class edaDF:
         a list of the names of the categorical columns
     num : list
         a list of the names of the numerical columns
+    outliers : pandas DataFrame
+        a DataFrame containing rows that correspond to each column with the low and high outliers naively generated 
 
     Methods
     -------
@@ -68,6 +67,14 @@ class edaDF:
         show : bool
             If true, display the graphs when the function is called. Otherwise the figure is returned. 
 
+    autoFeatures()
+        fills the cat and num variables with proper feature lists by dynamically obtaining rows that are 
+        category or object for cat, and int64 and float64 for num.
+
+    outlierCounter()
+        generates the DataFrame of outliers. It calculates the outliers by taking the power of 10 lower than the 
+        25th quartile as its low cutoff and the power of 10 higher than the 75th quartile as its high cutoff.
+
     fullEDA()
         Displays the full EDA process. 
     """
@@ -76,6 +83,8 @@ class edaDF:
         self.target = target
         self.cat = []
         self.num = []
+        self.autoFeatures()
+        self.outliers = self.outlierCounter()
 
     def info(self):
         return self.data.info()
@@ -89,44 +98,80 @@ class edaDF:
     def setNum(self, numList):
         self.num = numList
 
+    def debug(self):
+        print('Debuggerating')
+
+    def autoFeatures(self):
+        self.cat = list(self.data.select_dtypes(['object', 'category']).columns)
+        self.num = list(self.data.select_dtypes(['int64', 'float64']).columns)
+        if self.cat.count(self.target) > 0:
+            self.cat.remove(self.target)
+        if self.num.count(self.target) > 0:
+            self.num.remove(self.target)
+
     def countPlots(self, splitTarg=False, show=True):
         n = len(self.cat)
         cols = 2
-        figure, ax = plt.subplots(math.ceil(n/cols), cols)
-        r = 0
-        c = 0
-        for col in self.cat:
-            if splitTarg == False:
-                sns.countplot(data=self.data, x=col, ax=ax[r][c])
-            if splitTarg == True:
-                sns.countplot(data=self.data, x=col, hue=self.target, ax=ax[r][c])
-            c += 1
-            if c == cols:
-                r += 1
-                c = 0
-        if show == True:
-            figure.show()
-        return figure
+        if n != 0:
+            figure, ax = plt.subplots(math.ceil(n/cols), cols, figsize=(20,20))
+            r = 0
+            c = 0
+            for col in self.cat:
+                if splitTarg == False:
+                    sns.countplot(data=self.data, x=col, ax=ax[r][c])
+                if splitTarg == True:
+                    sns.countplot(data=self.data, x=col, hue=self.target, ax=ax[r][c])
+                c += 1
+                if c == cols:
+                    r += 1
+                    c = 0
+            if show == True:
+                figure.tight_layout()
+                figure.show()
+            return figure
+        else:
+            print("No categorical features in the data.")
 
     def histPlots(self, kde=True, splitTarg=False, show=True):
         n = len(self.num)
         cols = 2
-        figure, ax = plt.subplots(math.ceil(n/cols), cols)
-        r = 0
-        c = 0
+        if n != 0:
+            figure, ax = plt.subplots(math.ceil(n/cols), cols, figsize=(20,20))
+            r = 0
+            c = 0
+            for col in self.num:
+                #print("r:",r,"c:",c)
+                if splitTarg == False:
+                    sns.histplot(data=self.data, x=col, kde=kde, ax=ax[r][c])
+                if splitTarg == True:
+                    sns.histplot(data=self.data, x=col, hue=self.target, kde=kde, ax=ax[r][c])
+                c += 1
+                if c == cols:
+                    r += 1
+                    c = 0
+            if show == True:
+                figure.tight_layout()
+                figure.show()
+            return figure
+        else:
+            print("No numerical features in the data.")
+
+    def outlierCounter(self):
+        outliers = pd.DataFrame(columns=["Feature", "Low Outliers", "High Outliers"])
         for col in self.num:
-            #print("r:",r,"c:",c)
-            if splitTarg == False:
-                sns.histplot(data=self.data, x=col, kde=kde, ax=ax[r][c])
-            if splitTarg == True:
-                sns.histplot(data=self.data, x=col, hue=self.target, kde=kde, ax=ax[r][c])
-            c += 1
-            if c == cols:
-                r += 1
-                c = 0
-        if show == True:
-            figure.show()
-        return figure
+            lo = 0
+            ho = 0
+            loweval = self.data[col].quantile(0.25)*0.1
+            higheval = self.data[col].quantile(0.75)*10
+            if higheval == 0:
+                higheval = 1
+            for i in self.data[col]:
+                if i < loweval and i != 0:
+                    lo+=1
+                if i >= higheval:
+                    ho+=1
+            outliers = pd.concat([outliers,pd.Series({"Feature":col,"Low Outliers":lo,"High Outliers":ho}).to_frame().T], ignore_index=True)
+        return outliers
 
     def fullEDA(self):
         out1 = widgets.Output()
@@ -134,17 +179,25 @@ class edaDF:
         out3 = widgets.Output()
         out4 = widgets.Output()
         out5 = widgets.Output()
+        out6 = widgets.Output()
+        out7 = widgets.Output()
+        out8 = widgets.Output()
+        out9 = widgets.Output()
 
-        tab = widgets.Tab(children = [out1, out2, out3, out4, out5])
-        tab.set_title(0, 'Info')
-        tab.set_title(1, 'Categorical')
-        tab.set_title(2, 'Numereeeical')
-        tab.set_title(3, 'testing')
-        tab.set_title(4, 'teswssting')
+        tab = widgets.Tab(children=[out1,out2,out3,out4,out5,out6,out7,out8,out9])
+        tab.set_title(0,"Info")
+        tab.set_title(1,"Categorical")
+        tab.set_title(2,"Numerical")
+        tab.set_title(3,"Outlier Counts")
+        tab.set_title(4,"Descriptive Stats")
+        tab.set_title(5,"Value Counts")
+        tab.set_title(6,"Correlations")
+        tab.set_title(7,"Missing Values")
+        tab.set_title(8,"Pairplot")
         display(tab)
 
         with out1:
-            self.info()
+            display(pd.DataFrame(self.info()))
 
         with out2:
             fig2 = self.countPlots(splitTarg=True, show=False)
@@ -154,6 +207,26 @@ class edaDF:
             fig3 = self.histPlots(kde=True, show=False)
             plt.show(fig3)
 
-        with out4:
-            fig4 = self.histPlots(kde=True, show=False)
+        with out9:
+            fig4 = sns.pairplot(self.data)
             plt.show(fig4)
+        
+        with out5:
+            display(pd.DataFrame(self.data.describe()))
+
+        with out6:
+            for col in self.cat:
+                display(pd.DataFrame(self.data[col].value_counts()))
+                print('\n')
+
+        with out7:
+            corr = self.data.corr()
+            heat = sns.heatmap(corr, annot=True)
+            plt.show(heat)
+
+        with out8:
+            display(pd.DataFrame(self.data.isnull().sum()))
+
+        with out4:
+            print("These outliers are calculated by taking powers of 10 of the 25th and 75th quartiles, and are a guideline only")
+            display(self.outliers)
